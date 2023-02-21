e4c0f5b3de91e51664433f60256897438314d87d
rob grace <zwergie69@protonmail.com>
2023-02-21 13:22:17 +0100

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeHeroComponent } from './home-hero/home-hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeHeroComponent
   ],
   imports: [
     BrowserModule,
